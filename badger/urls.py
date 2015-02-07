4b70069cef744e58266d081fe00547ad43f1e570
arya <aboudaie@brandeis.edu>
2015-02-07 14:06:22 -0500

    added locations view

--- badger/urls.py
+++ badger/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import patterns, url
 from badger import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'scantron/(?P<name>\w+)',views.scantron,name='scantron'),
+    url(r'locations/',views.locations,name="locations")
     )
--- badger/views.py
+++ badger/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render,HttpResponse
 import json
-#test
+from django.core import serializers
+from models import Location
 # Create your views here.
 
 def index(request):
     d = {"hello":"world"}
     return HttpResponse(json.dumps(d), content_type='application/json')
+
+def locations(request):
+    data = serializers.serialize('json', Location.objects.all())
+    return HttpResponse(data, content_type='application/json')
+
+def scantron(request,name):
+    name = str(name.upper())
+    matrix =[[" " for x in range(len(name)+1)] for x in range(26+1)]
+    letters = [chr(i) for i in range(65,90+1)]
+    print letters
+    for i in range(len(name)):
+        if not (name[i])==" ":
+            j = letters.index(name[i])
+            matrix[j][i+1]="X"
+
+    for i in range(len(matrix)-1):
+        matrix[i][0] = letters[i]
+
+    matrix = [[" "]+list(range(1,len(matrix[0])))]+matrix
+    return HttpResponse(str(matrix))
