c1f05e2bf609b213e8089a10322cbe30f227126f
arya <aboudaie@brandeis.edu>
2015-02-07 12:01:37 -0500

    added admin page

--- badges/admin.py
+++ badges/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from badges.models import Badge,Location,BadgerUser
 
 # Register your models here.
+
+
+admin.site.register(Badge)
+admin.site.register(Location)
+admin.site.register(BadgerUser)
--- badges/models.py
+++ badges/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
+
+class Location(models.Model):
+    house_number = models.PositiveSmallIntegerField()
+    street = models.CharField(max_length = 30)
+    city = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 30)
+    rating = models.PositiveSmallIntegerField()
+    type = models.CharField(max_length = 30)
+    verified = models.BooleanField()
+    votes = models.IntegerField()
+
+class Badge(models.Model):
+    location = models.ForeignKey(Location)
+    description = models.CharField(max_length=200)
+    points = models.PositiveSmallIntegerField()
+    verified = models.BooleanField()
+    votes = models.IntegerField()
+
+
+class BadgerUser(models.Model):
+    user = models.OneToOneField(User)
+    badges = models.ManyToManyField(Badge)
