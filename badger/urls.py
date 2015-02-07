69e71800bc2ae633d0f4104921951f531d217670
arya <aboudaie@brandeis.edu>
2015-02-07 16:58:24 -0500

    fixed url

--- badger/urls.py
+++ badger/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     #url(r'scantron/(?P<name>\w+)',views.scantron,name='scantron'),
     url(r'locations/',views.locations,name="locations"),
-    url(r'locations/(\d+)/$',views.locations,name="single_location"),
+    url(r'locations/(?P<id>\d+)/$',views.single_location,name="single_location"),
     url(r'badges/',views.badges,name="badges"),
-
-    )
+    url(r'badges/(?P<id>\d+)/$',views.single_badge,name="single_badge"),
+)
