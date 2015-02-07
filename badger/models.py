c0a08429b278f7a33b4af2627e1a0cd851324391
arya <aboudaie@brandeis.edu>
2015-02-07 18:12:10 -0500

    commented out for now

--- badger/models.py
+++ badger/models.py
@@ -15,6 +15,7 @@ class Location(models.Model):
     type = models.CharField(max_length = 30)
     verified = models.BooleanField()
     votes = models.IntegerField()
+    #image = models.URLField(null=True)
 
     def __str__(self):
         return self.name
@@ -26,7 +27,7 @@ class Badge(models.Model):
     points = models.PositiveSmallIntegerField()
     verified = models.BooleanField()
     votes = models.IntegerField()
-    image = models.URLField(null=True)
+    #image = models.URLField(null=True)
     def __str__(self):
         return self.name
 
