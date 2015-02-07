0bc07c3a466fd37d02101a0d7b2134565a651e64
arya <aboudaie@brandeis.edu>
2015-02-07 18:05:55 -0500

    added link to badges, added characters to limit

--- badger/models.py
+++ badger/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Location(models.Model):
     house_number = models.PositiveSmallIntegerField()
-    street = models.CharField(max_length = 30)
+    street = models.CharField(max_length = 70)
     city = models.CharField(max_length = 30)
     name = models.CharField(max_length = 30)
     rating = models.PositiveSmallIntegerField()
@@ -20,12 +20,13 @@ class Location(models.Model):
         return self.name
 
 class Badge(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
     location = models.ForeignKey(Location)
     description = models.CharField(max_length=200)
     points = models.PositiveSmallIntegerField()
     verified = models.BooleanField()
     votes = models.IntegerField()
+    image = models.URLField(null=True)
     def __str__(self):
         return self.name
 
